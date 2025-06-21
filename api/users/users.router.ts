bbf68c8096a6c22bd9c69ce2c91fcec308e35d72
z1rowww <zxcursed505@gmail.com>
2025-06-21 09:33:59 +0300

    commit

--- api/users/users.router.ts
+++ api/users/users.router.ts
@@ -1,21 +1,16 @@
 import express, { RequestHandler } from 'express';
 import * as usersController from './users.controller';
 import passport from 'passport';
-import { getUsersWithCursor } from './users.controller';
 
 const router = express.Router();
 
 router.post('/register', usersController.register);
-router.post(
-  '/login',
-  passport.authenticate('local'),
-  usersController.login as RequestHandler,
-);
+router.post('/login', passport.authenticate('local'), usersController.login as RequestHandler);
 router.post('/insert-many', usersController.insertManyUsers);
 router.post('/insert-one', usersController.insertOne);
 
 router.delete('/delete-one', usersController.deleteOne);
-router.delete('/delete-many', usersController.deleteOne);
+router.delete('/delete-many', usersController.deleteMany);
 
 router.patch('/update-one', usersController.updateOne);
 router.patch('/update-many', usersController.updateMany);
@@ -25,7 +20,6 @@ router.get('/userslist', usersController.getAllUsers);
 router.get('/profile', usersController.getUserInfo);
 router.get('/logout', usersController.logout);
 router.get('/projection', usersController.findUsersWithProjection);
-
 router.get('/cursor', usersController.getUsersWithCursor);
 router.get('/stats', usersController.getUserStats);
 
